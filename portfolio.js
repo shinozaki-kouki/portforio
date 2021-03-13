d9a81b8f6f0240c413ea2a5f72b02e3bfe21fb4b
shinozaki-kouki <shinozaki0605@icloud.com>
2021-03-13 21:34:06 +0900

    ファイルの追加とタイトルの変更

---
+++ portfolio.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    ScrollReveal().reveal('.scroll',{
+        duration:"1500",
+        reset: true
+    });   
+    ScrollReveal().reveal('.box',{ 
+        duration: "1500", 
+        viewFactor: 0.2, 
+        reset: true,
+        origin:"left",
+        distance:"150px",
+    });
+    ScrollReveal().reveal('.introduce',{ 
+        duration: "1000", 
+        viewFactor: 0.2, 
+        reset: true,
+        delay:"1000",
+    });
+    });
+                
