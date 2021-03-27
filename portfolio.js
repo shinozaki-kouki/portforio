d624f5c4ac1d428f85824f0c16a9fa4334329401
shinozaki-kouki <shinozaki0605@icloud.com>
2021-03-27 18:31:11 +0900

    アニメーション、問い合わせフォーム調整

--- portfolio.js
+++ portfolio.js
@@ -1,19 +1,19 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     ScrollReveal().reveal('.scroll',{
         duration:"1500",
-        reset: true
+        // reset: true
     });   
     ScrollReveal().reveal('.box',{ 
         duration: "1500", 
         viewFactor: 0.2, 
-        reset: true,
+        // reset: true,
         origin:"left",
         distance:"150px",
     });
     ScrollReveal().reveal('.introduce',{ 
         duration: "1000", 
         viewFactor: 0.2, 
-        reset: true,
+        // reset: true,
         delay:"1000",
     });
     });
