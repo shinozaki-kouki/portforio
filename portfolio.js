fe0d06cdd5566ef39dba384220925a983d9b8c22
shinozaki-kouki <shinozaki0605@icloud.com>
2021-03-28 12:15:28 +0900

    レスポンシブファイルの追加、各ページクラス調整

--- portfolio.js
+++ portfolio.js
@@ -1,18 +1,19 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     ScrollReveal().reveal('.scroll',{
         duration:"1500",
+        viewFactor: 0.7, 
         reset: false,
     });   
     ScrollReveal().reveal('.box',{ 
         duration: "1500", 
-        viewFactor: 0.2, 
+        viewFactor: 0.6, 
         reset: false,
         origin:"left",
         distance:"150px",
     });
     ScrollReveal().reveal('.introduce',{ 
         duration: "1000", 
-        viewFactor: 0.2, 
+        viewFactor: 0.6, 
         reset: false,
         delay:"800",
     });
