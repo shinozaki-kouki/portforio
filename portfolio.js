120507c803a900df0a84dfade75237da928cabb9
shinozaki-kouki <shinozaki0605@icloud.com>
2021-03-30 20:36:02 +0900

    全ページレスポンシブデザイン導入

--- portfolio.js
+++ portfolio.js
@@ -1,21 +1,32 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     ScrollReveal().reveal('.scroll',{
         duration:"1000",
-        viewFactor: 0.4, 
+        viewFactor: 0.6, 
         reset: false,
     });   
-    ScrollReveal().reveal('.box',{ 
-        duration: "1500", 
+    // ScrollReveal().reveal('.box',{ 
+    //     duration: "1500", 
+    //     viewFactor: 0.6, 
+    //     reset: false,
+    //     origin:"left",
+    //     distance:"150px",
+    // });
+    // ScrollReveal().reveal('.introduce',{ 
+    //     duration: "1000", 
+    //     viewFactor: 0.6, 
+    //     reset: false,
+    //     delay:"800",
+    // });
+    ScrollReveal().reveal('.introduce',{
+        duration:"1000",
         viewFactor: 0.6, 
         reset: false,
-        origin:"left",
-        distance:"150px",
-    });
-    ScrollReveal().reveal('.introduce',{ 
-        duration: "1000", 
-        viewFactor: 0.6, 
+    });   
+    ScrollReveal().reveal('.box',{
+        duration:"1000",
+        viewFactor: 0.8, 
         reset: false,
         delay:"800",
-    });
+    });   
     });
                 
