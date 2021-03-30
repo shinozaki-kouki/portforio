d426f2874f485ce4eed51d25610706e9e943bd0f
shinozaki-kouki <shinozaki0605@icloud.com>
2021-03-30 21:16:37 +0900

    レスポンシブデザイン訂正

--- portfolio.js
+++ portfolio.js
@@ -1,32 +1,18 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     ScrollReveal().reveal('.scroll',{
         duration:"1000",
-        viewFactor: 0.6, 
+        viewFactor: 0.3, 
         reset: false,
     });   
-    // ScrollReveal().reveal('.box',{ 
-    //     duration: "1500", 
-    //     viewFactor: 0.6, 
-    //     reset: false,
-    //     origin:"left",
-    //     distance:"150px",
-    // });
-    // ScrollReveal().reveal('.introduce',{ 
-    //     duration: "1000", 
-    //     viewFactor: 0.6, 
-    //     reset: false,
-    //     delay:"800",
-    // });
     ScrollReveal().reveal('.introduce',{
         duration:"1000",
-        viewFactor: 0.6, 
+        viewFactor: 0.3, 
         reset: false,
     });   
     ScrollReveal().reveal('.box',{
         duration:"1000",
-        viewFactor: 0.8, 
+        viewFactor: 0.3, 
         reset: false,
-        delay:"800",
     });   
     });
                 
