83b435b21f35629fbbbf13dff282206c47437a09
shinozaki-kouki <shinozaki0605@icloud.com>
2021-04-02 23:47:07 +0900

    ページトップボタンjQuery導入

--- portfolio.js
+++ portfolio.js
@@ -16,6 +16,21 @@ window.addEventListener('DOMContentLoaded', (event) => {
         viewFactor: 0.3, 
         reset: false,
         mobile: false,
-    });   
+    });  
     });
-                
+    $(function() {
+        var pagetop = $('#js-scroll-fadein');   
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) { 
+              pagetop.addClass("is-fadein");
+            } else {
+              pagetop.removeClass("is-fadein");
+            }
+        });
+        pagetop.on("click",function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 500); 
+            return false;
+        });
+      });
