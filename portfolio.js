5adc0320259cd3ff98fe14df074d18164a5042c6
shinozaki-kouki <shinozaki0605@icloud.com>
2021-04-08 18:52:12 +0900

    work訂正

--- portfolio.js
+++ portfolio.js
@@ -1,26 +1,26 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     ScrollReveal().reveal('.scroll',{
         duration:"1000",
-        viewFactor: 0.3, 
+        viewFactor: 0.3,
         reset: false,
         mobile: false,
-    });   
+    });
     ScrollReveal().reveal('.introduce',{
         duration:"1000",
-        viewFactor: 0.3, 
+        viewFactor: 0.3,
         reset: false,
         mobile: false,
-    });   
+    });
     ScrollReveal().reveal('.box',{
         duration:"1000",
-        viewFactor: 0.3, 
+        viewFactor: 0.3,
         reset: false,
         mobile: false,
-    });  
+    });
     $(function() {
-        var pagetop = $('#js-scroll-fadein');   
+        var pagetop = $('#js-scroll-fadein');
         $(window).scroll(function () {
-            if ($(this).scrollTop() > 100) { 
+            if ($(this).scrollTop() > 100) {
               pagetop.addClass("is-fadein");
             } else {
               pagetop.removeClass("is-fadein");
@@ -29,19 +29,18 @@ window.addEventListener('DOMContentLoaded', (event) => {
         pagetop.on("click",function () {
             $('body,html').animate({
                 scrollTop: 0
-            }, 500); 
+            }, 500);
             return false;
         });
-      });
-      $(window).resize(function(){
-        let x = $(window).width();
-        let y = 1024;
-        if (y <= x) {
-          $('.push').addClass('box2 box3');
-        }
-        else {
-          $('.push').removeClass('box2 box3');
-        }
-      });
     });
-    
+    $(window).resize(function(){
+      let x = $(window).width();
+      let y = 1024;
+      if (y <= x) {
+        $('.push').addClass('box2 box3');
+      }
+      else {
+        $('.push').removeClass('box2 box3');
+      }
+    });
+});
